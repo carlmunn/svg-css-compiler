bbde84057dee6c532d107e3d12a9ed2d6d029f3d
Carl Munn <carl.munn@open2view.com>
2016-08-05 22:20:13 +1200

    Basic start to structure
    - Mainly with creating tests
    - Added SVG test files to work with

--- test/test.js
+++ test/test.js
@@ -1,18 +1,81 @@
-var MainApp = require('../lib/main');
-var assert  = require('assert');
+const path    = require('path');
 
-describe('svn-css-combiner', _=>{
+const MainApp = require('../lib/main');
+const assert  = require('assert');
+
+function basePath(loc) {
+  return path.join(path.dirname(__filename), loc);
+}
+
+describe('Test with without fresh load', _=>{
   
-  var app;
+  var mainSVG;
 
   before(()=>{
-    app = new MainApp('path');
+    mainSVG = new MainApp('path');
   });
 
   it('Should check #files', ()=>{
-    assert(!app.files.length);
-    app.add('./');
-    //console.info('files', app.files);
-    //assert(app.files.length);
+    assert(!mainSVG.files().length);
+    mainSVG.add(basePath('files'));
+    assert.equal(mainSVG.files().length, 5);
+  });
+
+  it('should check #content', ()=>{
+    mainSVG.add(basePath('files'));
+    assert(mainSVG.files(0).content.length > 10);
+  });
+
+  it('should check #dom', ()=>{
+    mainSVG.add(basePath('files'));
+    assert(mainSVG.files(0).dom instanceof Object);
+  });
+
+  it('should check the viewBoxes', ()=>{
+    mainSVG.add(basePath('files'));
+    assert.equal(mainSVG.files(0).viewBox(), '0 0 36 33');
+  });
+
+  it('should check the viewBoxes', ()=>{
+    mainSVG.add(basePath('files'));
+
+    const pos = mainSVG.files(0).position();
+
+    assert.equal(pos.x, 0);
+    assert.equal(pos.y, 0);
+    assert.equal(pos.width, 36);
+    assert.equal(pos.height, 33);
+  });
+});
+
+describe('Test with fresh load', _=>{
+
+  beforeEach(()=>{
+    mainSVG = new MainApp('path');
+  });
+
+  it('should check the SVG total height/width', ()=>{
+    mainSVG.add(basePath('files'));
+
+    assert.equal(mainSVG.height(), 161);
+    assert.equal(mainSVG.width(), 164);
+  });
+
+  it.only('should check the file symbol element', ()=>{
+    mainSVG.add(basePath('files'));
+
+    console.info('====>', mainSVG.files(0)._svgSymbol())
+
+    //assert.equal(mainSVG.height(), 161);
+    //assert.equal(mainSVG.width(), 164);
+  });
+
+  it.only('should check the file use element', ()=>{
+    mainSVG.add(basePath('files'));
+
+    console.info('====>', mainSVG.files(0)._svgUse())
+
+    //assert.equal(mainSVG.height(), 161);
+    //assert.equal(mainSVG.width(), 164);
   });
 });
