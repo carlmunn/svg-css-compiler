c7a38c705e89f28cf15542fe3e92b690593f7118
Carl Munn <me@carlmunn.com>
2016-08-08 21:37:36 +1200

    WIP - Generates working SVG file
    - Simple but it works for now
    - Look at implementing packing algo later

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const path    = require('path');
+//const cheerio = require('cheerio');
 
 const MainApp = require('../lib/main');
 const assert  = require('assert');
@@ -9,37 +10,37 @@ function basePath(loc) {
 
 describe('Test with without fresh load', _=>{
   
-  var mainSVG;
+  var mainSvg;
 
   before(()=>{
-    mainSVG = new MainApp('path');
+    mainSvg = new MainApp('path');
   });
 
   it('Should check #files', ()=>{
-    assert(!mainSVG.files().length);
-    mainSVG.add(basePath('files'));
-    assert.equal(mainSVG.files().length, 5);
+    assert(!mainSvg.files().length);
+    mainSvg.add(basePath('files'));
+    assert.equal(mainSvg.files().length, 5);
   });
 
   it('should check #content', ()=>{
-    mainSVG.add(basePath('files'));
-    assert(mainSVG.files(0).content.length > 10);
+    mainSvg.add(basePath('files'));
+    assert(mainSvg.files(0).content.length > 10);
   });
 
   it('should check #dom', ()=>{
-    mainSVG.add(basePath('files'));
-    assert(mainSVG.files(0).dom instanceof Object);
+    mainSvg.add(basePath('files'));
+    assert(mainSvg.files(0).dom instanceof Object);
   });
 
   it('should check the viewBoxes', ()=>{
-    mainSVG.add(basePath('files'));
-    assert.equal(mainSVG.files(0).viewBox(), '0 0 36 33');
+    mainSvg.add(basePath('files'));
+    assert.equal(mainSvg.files(0).viewBox(), '0 0 36 33');
   });
 
   it('should check the viewBoxes', ()=>{
-    mainSVG.add(basePath('files'));
+    mainSvg.add(basePath('files'));
 
-    const pos = mainSVG.files(0).position();
+    const pos = mainSvg.files(0).position();
 
     assert.equal(pos.x, 0);
     assert.equal(pos.y, 0);
@@ -51,31 +52,35 @@ describe('Test with without fresh load', _=>{
 describe('Test with fresh load', _=>{
 
   beforeEach(()=>{
-    const mainSVG = new MainApp('path');
+    mainSvg = new MainApp('path');
   });
 
   it('should check the SVG total height/width', ()=>{
-    mainSVG.add(basePath('files'));
+    mainSvg.add(basePath('files'));
 
-    assert.equal(mainSVG.height(), 161);
-    assert.equal(mainSVG.width(), 164);
+    assert.equal(mainSvg.height(), 161);
+    assert.equal(mainSvg.width(), 164);
   });
 
-  it.only('should check the file symbol element', ()=>{
-    mainSVG.add(basePath('files'));
-
-    console.info('====>', mainSVG.files(0)._svgSymbol())
-
-    //assert.equal(mainSVG.height(), 161);
-    //assert.equal(mainSVG.width(), 164);
+  it('should check the file symbol element', ()=>{
+    mainSvg.add(basePath('files'));
+    assert(typeof(mainSvg.files(0)._svgSymbol()) == 'string');
   });
 
-  it.only('should check the file use element', ()=>{
-    mainSVG.add(basePath('files'));
-
-    console.info('====>', mainSVG.files(0)._svgUse())
-
-    //assert.equal(mainSVG.height(), 161);
-    //assert.equal(mainSVG.width(), 164);
+  it('should check the file use element', ()=>{
+    mainSvg.add(basePath('files'));
+    assert(typeof(mainSvg.files(0)._svgUse()) == 'string');
   });
+
+  it.only('should test #svg', ()=>{
+    mainSvg.add(basePath('files'));
+
+    //console.info('SVG', mainSvg.toSvg());
+
+    mainSvg.toFile();
+  });
+
+  describe('test Sort');
+
+  describe('test Group');
 });
